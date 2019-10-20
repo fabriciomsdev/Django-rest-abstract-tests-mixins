00bc6027fbfb01c9b75ed239ef378332a1f01e13
Fabricio Magalhães <fabricioms.dev@gmail.com>
2019-10-20 18:27:36 -0300

    Begin

---
+++ tests.py
@@ -0,0 +1,95 @@
+from django.test import TestCase, Client
+from rest_framework import status
+from rest_framework.test import APIClient
+from backend.settings import AUTH_USER_MODEL
+from django.apps import apps
+import json
+import abc
+
+
+class GenericDRFTestResource():
+    data_for_test = {}
+    data_updated_for_test = {}
+    url = ''
+    user_model = apps.get_model(*AUTH_USER_MODEL.split('.'))
+    detail_of_item_stored = {}
+    attr_pk = 'id'
+
+    def setUp(self):
+        self.client = APIClient()
+        self.create_an_user()
+        self.auth_user = self.user_model.objects.all()[0]
+
+    def build_url(self, params=None, pk=None):
+        url = self.url
+        if params:
+            url.format(params)
+
+        if pk:
+            url += str(pk) + '/'
+
+        return url
+
+    def create_an_user(self, is_staff=False):
+        self.admin = self.user_model(
+            username='testuser',
+            password='password',
+            is_staff=is_staff)
+        self.admin.save()
+
+    def create_an_item(self):
+        response = self.client.post(self.build_url(), self.data_for_test)
+        self.detail_of_item_stored = json.loads(response.content)
+        return self
+
+    def do_login(self):
+        self.client.force_authenticate(user=self.auth_user)
+
+        return self
+
+
+class TestResourceRouteExist(GenericDRFTestResource):
+    def tests_if_route_exists(self):
+            response = self.client.get(self.build_url())
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestUserNotLoggedCanNotPost(GenericDRFTestResource):
+    def tests_if_user_not_logged_can_not_publish(self):
+        """
+            If user wasn't make login he cann't publish a post
+        """
+        response = self.client.post(self.build_url(), self.data_for_test)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class TestUserLoggedCanPost(GenericDRFTestResource):
+    def tests_if_user_logged_can_publish(self):
+            self.do_login()
+
+            response = self.client.post(self.build_url(), self.data_for_test)
+            self.detail_of_item_stored = response.content
+
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class TestUserLoggedCanUpdate(GenericDRFTestResource):
+    def tests_if_user_logged_can_update(self):
+        self.do_login() \
+            .create_an_item()
+
+        url = self.build_url(pk=self.detail_of_item_stored.get(self.attr_pk))
+        response = self.client.put(url, self.data_updated_for_test)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestUserLoggedCanDelete(GenericDRFTestResource):
+    def tests_if_user_logged_can_delete(self):
+        self.do_login() \
+            .create_an_item()
+
+        url = self.build_url(pk=self.detail_of_item_stored.get(self.attr_pk))
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
